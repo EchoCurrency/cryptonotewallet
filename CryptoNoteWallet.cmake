ca9b5c560b8ef5749d4d97a6eecff05ead466711
ubeweb <34456359+ubeweb@users.noreply.github.com>
2018-01-14 13:43:08 -0500

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "echo-currency Wallet")
+set(CN_CURRENCY_DISPLAY_NAME "echo")
+set(CN_CURRENCY_TICKER "echo")
